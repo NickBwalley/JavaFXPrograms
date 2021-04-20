1af3e08a699953a2c4c30b7edc728d46289b4d8a
NickBwalley <nickbwalley@gmail.com>
2021-04-20 10:26:29 +0300

    JavaFX CreateBasicWindow

--- CreatingBasicWindow/src/sample/Main.java
+++ CreatingBasicWindow/src/sample/Main.java
@@ -1,23 +1,41 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main extends Application{
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    Button button;
+
+    public static void main(String[] args){
+        launch(args); // whenever your program starts its going to call the launch so the launch() goes to the
+            // application class set everything up and then call the start
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    // main javafx code
+        /*NOTE:
+        * In JavaFX the entire window is called a STAGE
+        * The content inside your window or inside the STAGE is called the SCENE
+        *   and on the scene is where we put all the cool stuff like buttons, widgets etc */
+        primaryStage.setTitle("Title of the Window"); // set title of the window;
 
-    public static void main(String[] args) {
-        launch(args);
+        button = new Button();
+        button.setText("Click me");
+
+        StackPane layout = new StackPane(); // position the button in the middle
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
     }
 }
