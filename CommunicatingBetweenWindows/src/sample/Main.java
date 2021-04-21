677738f530297720c63294a5a97f022566514036
NickBwalley <nickbwalley@gmail.com>
2021-04-21 11:11:29 +0300

    JavaFX CommunicatingBetweenWindows

---
+++ CommunicatingBetweenWindows/src/sample/Main.java
@@ -0,0 +1,31 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+public class Main  extends Application{
+    Stage window;
+    Button button;
+
+    public static void main (String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        window = primaryStage;
+        window.setTitle("MrBeast!");
+        button = new Button("Click me!");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+        Scene scene = new Scene(layout, 300, 250);
+        window.setScene(scene);
+        window.show();
+    }
+}
