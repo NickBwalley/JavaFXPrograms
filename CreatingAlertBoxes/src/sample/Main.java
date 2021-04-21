a63e5b45b6c0906778841b89c4d26c908070466f
NickBwalley <nickbwalley@gmail.com>
2021-04-21 10:33:12 +0300

    JavaFX CreatingAlertBoxes

--- CreatingAlertBoxes/src/sample/Main.java
+++ CreatingAlertBoxes/src/sample/Main.java
@@ -1,23 +1,30 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import  javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-
-
-    public static void main(String[] args) {
+    Stage window;
+    Button button;
+    public static void main(String[] args){
         launch(args);
     }
+
+    public void start(Stage primaryStage) throws Exception{
+        window = primaryStage;
+        window.setTitle("MrBeast Shop");
+
+        button = new Button("Click me!");
+        StackPane layout  = new StackPane();
+        layout.getChildren().add(button);
+        Scene scene = new Scene(layout, 300, 250);
+        window.setScene(scene);
+        window.show();
+
+    }
+
 }
