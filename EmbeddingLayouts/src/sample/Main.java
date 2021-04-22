93133c9c115aa7761ee6ae1ac8163f0f908968e6
NickBwalley <nickbwalley@gmail.com>
2021-04-22 11:33:37 +0300

    JavaFX EmbeddingLayouts

--- EmbeddingLayouts/src/sample/Main.java
+++ EmbeddingLayouts/src/sample/Main.java
@@ -1,23 +1,44 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+
+public class Main  extends Application{
+    Stage window;
+
+
+    public static void main (String[] args){
+        launch(args);
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        window = primaryStage;
+        window.setTitle("MrBeast!");
+
+        HBox topMenu = new HBox();
+        Button buttonA = new Button("File");
+        Button buttonB = new Button("Edit");
+        Button buttonC = new Button("View");
+        topMenu.getChildren().addAll(buttonA, buttonB, buttonC);
+
+        VBox leftMenu = new VBox();
+        Button buttonD = new Button("D");
+        Button buttonE = new Button("E");
+        Button buttonF = new Button("F");
+        leftMenu.getChildren().addAll(buttonD, buttonE, buttonF);
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setTop(topMenu);
+        borderPane.setLeft(leftMenu);
+
+        Scene scene = new Scene(borderPane, 300, 250);
+        window.setScene(scene);
+        window.show();
     }
 
-
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
