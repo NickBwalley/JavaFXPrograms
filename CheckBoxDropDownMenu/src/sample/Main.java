e3128c9fa644a493677015fe904df143eaf0d9f8
NickBwalley <nickbwalley@gmail.com>
2021-04-23 22:04:49 +0300

    JavaFX CheckBoxDropDownMenu

--- CheckBoxDropDownMenu/src/sample/Main.java
+++ CheckBoxDropDownMenu/src/sample/Main.java
@@ -2,11 +2,14 @@ package sample;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+
+import java.awt.*;
 
 
 public class Main extends Application{
@@ -17,18 +20,37 @@ public class Main extends Application{
 
     public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("ShopMrBeast!");
-        CheckBox box1 = new CheckBox("Ham");
-        CheckBox box2 = new CheckBox("Chicken");
+
+        ChoiceBox<String> choiceBox = new ChoiceBox<>();
+
+        //getItems returns the ObservableList object which you can add items to
+        choiceBox.getItems().add("Apples");
+        choiceBox.getItems().add("Bananas");
+        choiceBox.getItems().add("Papaya");
+        choiceBox.getItems().addAll("Bacon", "Ham", "Chicken");
+
+        // Set Default Value
+        choiceBox.setValue("Apples");
 
         // Button
         Button button1 = new Button("Order Now!");
+        button1.setOnAction(e -> getChoice(choiceBox));
+
+        // Layout
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20,20,20,20));
-        layout.getChildren().addAll(box1, box2, button1);
+        layout.getChildren().addAll(choiceBox, button1);
 
         // Scene
         Scene scene = new Scene(layout, 300, 250);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+
+    // Handle ChoiceBox
+    private void getChoice(ChoiceBox<String> choiceBox){
+        String food = choiceBox.getValue();
+        System.out.println(food);
+    }
+
 }
